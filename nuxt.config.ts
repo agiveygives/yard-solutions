2396876b8acdb657b3092dab511f0bd2a47b7aae
Andrew Givens <justincase25xxv@gmail.com>
2025-01-02 02:44:37 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 
 export default defineNuxtConfig({
+  ssr: false,
   components: true,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
