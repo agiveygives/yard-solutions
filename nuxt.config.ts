7365403cd025f1fb1ca0d6ac300c9feacf819752
Andrew Givens <justincase25xxv@gmail.com>
2025-01-02 02:22:54 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', 'nuxtjs-naive-ui', '@nuxt/icon'],
+  nitro: {
+    preset: 'netlify',
+  },
   vite: {
     plugins: [
       AutoImport({
