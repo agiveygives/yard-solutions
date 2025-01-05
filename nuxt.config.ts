c97d5a23f98c292329a07f14426b59eafb8722c6
agiveygives <justincase25xxv@gmail.com>
2025-01-05 15:40:23 -0600

    nuxt seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,22 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 
 export default defineNuxtConfig({
-  ssr: false,
+  target: 'static',
+  ssr: true,
   components: true,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'nuxtjs-naive-ui', '@nuxt/icon', '@nuxt/fonts', '@nuxt/ui'],
+  modules: [
+    '@nuxt/eslint',
+    'nuxtjs-naive-ui',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/ui',
+    '@nuxtjs/seo'
+  ],
   nitro: {
     preset: 'netlify',
-    debug: 'true',
+    debug: true,
   },
   vite: {
     plugins: [
@@ -37,5 +45,28 @@ export default defineNuxtConfig({
   },
   css: [
     '@/components/Layout/Navigation.vue'
-  ]
+  ],
+  site: {
+    // Default meta tags
+    baseUrl: 'https://yardsolutionskc.com',
+    name: 'Yard Solutions LLC',
+    title: 'Yard Solutions LLC',
+    templateTitle: '%name% | %title%',
+    description: 'Affordable Lawn Care in the Kansas City Metro Area',
+    canonical: true, // Automatically generate canonical tags
+    author: 'Yard Solutions LLC',
+
+    // Open Graph settings
+    ogType: 'website',
+    ogTitle: 'Yard Solutions LLC',
+    ogDescription: 'Affordable Lawn Care in the Kansas City Metro Area',
+    ogImage: '/default-og-image.jpg', // Default Open Graph image
+    ogHost: 'https://yardsolutionskc.com',
+
+    // Robots settings
+    robots: {
+      index: true,
+      follow: true,
+    },
+  },
 })
